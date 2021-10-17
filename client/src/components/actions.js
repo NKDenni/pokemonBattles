982786171307b6a9cf1f0b5d7e3f099c268e547f
Matias Salas <matias.salas74@gmail.com>
2021-10-17 11:25:53 -0700

    Add Game logic, game context. Added axios module to client code base. Updated Actions and App components to provide example of how to consume the game context.

--- client/src/components/actions.js
+++ client/src/components/actions.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './actions.scss';
-
+import { useGame } from '../context/GameContext';
 
 const Actions = (props) => {
+    const { play, score } = useGame();
+    const [winner, setWinner] = useState('');
+
+    const handlePlay = (e) => {
+        setWinner('');
+        const turnWinner = play('paper');
+        setWinner(`winner: ${turnWinner.winner} score: ${score}`);
+    }
 
     return (
         <div className="d-flex flex-column justify-content-center">
+            { winner }
             <div className="row col my-2">
                 <button className="text-center">Change Appearance</button>
             </div>
             <div className="row col my-2">
-                <button className="text-center">Battle</button>
+                <button className="text-center" onClick={ handlePlay }>Battle</button>
             </div>
             <div className="row col my-2">
                 <button className="text-center">Learn Moves</button>
