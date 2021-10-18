dbdcf35009d454a85c8d98f5cf7a612c12f82850
Nathan Denni <aubakasan303@gmail.com>
2021-10-18 22:56:46 +0900

    Learn page layout completed and battle page

--- client/src/components/learn.js
+++ client/src/components/learn.js
@@ -9,14 +9,14 @@ const Battle = (props) => {
             <div className="col-sm-6">
                 <div className="row d-flex align-items-around align-content-around justify-content-center">
                     <div className="row px-5">
-                        <img className="text-center monster" src={'./img/pokemon/4-pikachu.png'} alt="Pikachu" />
+                        <img className="text-center monster" src={'./img/pokemon/2-charmander.png'} alt="Charmander" />
                         <div className="col d-flex flex-column justify-content-evenly">
                             <div className="row">
-                                <p className="col text-start pe-0 mb-1">Type: Electric{props.type}</p>
-                                <p className="col-sm-4 text-end p-0 mb-1">#: 25{props.number}</p>
+                                <p className="col text-start pe-0 mb-1">Type: Fire{props.type}</p>
+                                <p className="col-sm-4 text-end p-0 mb-1">#: 4{props.number}</p>
                             </div>
                             <div className="row">
-                                <p className="col text-start pe-0 mb-1">HP: 35{props.health}</p>
+                                <p className="col text-start pe-0 mb-1">HP: 39{props.health}</p>
                                 <p className="col text-end p-0 mb-1">Speed: 50{props.speed}</p>
                             </div>
                             <div className="row">
@@ -42,8 +42,8 @@ const Battle = (props) => {
                             <hr className="mt-2 mb-1" />
                         </div>
                         <div className="row d-flex p-0 m-0 flex-row">
-                            <p className="col-sm-5 text-start p-0 mb-0">Thunder shock{props.move2}</p>
-                            <p className="col-sm-4 text-center p-0 mb-0">Power: 50{props.attack2}</p>
+                            <p className="col-sm-5 text-start p-0 mb-0">Scratch{props.move2}</p>
+                            <p className="col-sm-4 text-center p-0 mb-0">Power: 40{props.attack2}</p>
                             <p className="col-sm-3 text-end p-0 mb-0">Acc: 100{props.accuracy2}</p>
                         </div>
                         <div className="px-0">
@@ -68,9 +68,9 @@ const Battle = (props) => {
                             <hr className="mt-2 mb-2" />
                         </div>
                         <div className="row d-flex p-0 m-0 flex-row justify-content-center">
-                            <p className="col-sm-4 text-start p-0 mb-0">Height: 4{props.height}</p>
-                            <p className="col-sm-4 text-center p-0 mb-0">Lvl: 1{props.level}</p>
-                            <p className="col-sm-4 text-end p-0 mb-0">Weight: 50{props.weight}</p>
+                            <p className="col-sm-4 text-start p-0 mb-0">Height: 6{props.height}</p>
+                            <p className="col-sm-4 text-center p-0 mb-0">Lvl: 9{props.level}</p>
+                            <p className="col-sm-4 text-end p-0 mb-0">Weight: 85{props.weight}</p>
                         </div>
                     </div>
                 </div>
@@ -78,68 +78,60 @@ const Battle = (props) => {
             <div className="col-sm-6">
                 <div className="row d-flex align-items-around align-content-around justify-content-center">
                     <div className="row px-5">
-                        <img className="text-center monster" src={'./img/pokemon/4-pikachu.png'} alt="Pikachu" />
-                        <div className="col d-flex flex-column justify-content-evenly">
-                            <div className="row">
-                                <p className="col text-start pe-0 mb-1">Type: Electric{props.type}</p>
-                                <p className="col-sm-4 text-end p-0 mb-1">#: 25{props.number}</p>
-                            </div>
-                            <div className="row">
-                                <p className="col text-start pe-0 mb-1">HP: 35{props.health}</p>
-                                <p className="col text-end p-0 mb-1">Speed: 50{props.speed}</p>
-                            </div>
-                            <div className="row">
-                                <p className="col text-start pe-0 mb-1">Attack: 50{props.attack}</p>
-                                <p className="col text-end p-0 mb-1">Defense: 40{props.defense}</p>
-                            </div>
-                            <div className="row">
-                                <p className="col text-start pe-0 mb-1">S-Att: 50{props.satt}</p>
-                                <p className="col text-end p-0 mb-1">S-Def: 50{props.sdef}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row px-5">
-                        <div className="px-0">
-                            <hr className="mt-0 mb-1" />
-                        </div>
+                        <h1 className="mb-5">Moves You can Learn</h1>
                         <div className="row d-flex p-0 m-0 flex-row">
-                            <p className="col-sm-5 text-start p-0 mb-0">Growl{props.move1}</p>
-                            <p className="col-sm-4 text-center p-0 mb-0">Power: --{props.attack1}</p>
-                            <p className="col-sm-3 text-end p-0 mb-0">Acc: 100{props.accuracy1}</p>
+                            <p className="col-sm-2 text-start p-0 mb-0">Lvl: 9{props.movelvl1}</p>
+                            <p className="col text-start p-0 mb-0">Ember {props.movename1}</p>
+                            <p className="col text-center p-0 mb-0">Power: 40{props.attack1}</p>
+                            <p className="col text-end p-0 mb-0">Acc: 100{props.accuracy1}</p>
                         </div>
                         <div className="px-0">
                             <hr className="mt-2 mb-1" />
                         </div>
                         <div className="row d-flex p-0 m-0 flex-row">
-                            <p className="col-sm-5 text-start p-0 mb-0">Thunder shock{props.move2}</p>
-                            <p className="col-sm-4 text-center p-0 mb-0">Power: 50{props.attack2}</p>
-                            <p className="col-sm-3 text-end p-0 mb-0">Acc: 100{props.accuracy2}</p>
+                            <p className="col-sm-2 text-start p-0 mb-0">Lvl: 15{props.movelvl2}</p>
+                            <p className="col text-start p-0 mb-0">Leer {props.movename2}</p>
+                            <p className="col text-center p-0 mb-0">Power: --{props.attack2}</p>
+                            <p className="col text-end p-0 mb-0">Acc: 100{props.accuracy2}</p>
                         </div>
                         <div className="px-0">
                             <hr className="mt-2 mb-1" />
                         </div>
                         <div className="row d-flex p-0 m-0 flex-row">
-                            <p className="col-sm-5 text-start p-0 mb-0">Move{props.move3}</p>
-                            <p className="col-sm-4 text-center p-0 mb-0">{props.attack3}</p>
-                            <p className="col-sm-3 text-end p-0 mb-0">{props.accuracy3}</p>
+                            <p className="col-sm-2 text-start p-0 mb-0">Lvl: 22{props.movelvl3}</p>
+                            <p className="col text-start p-0 mb-0">Rage {props.movename3}</p>
+                            <p className="col text-center p-0 mb-0">Power: 20{props.attack3}</p>
+                            <p className="col text-end p-0 mb-0">Acc: 100{props.accuracy3}</p>
                         </div>
                         <div className="px-0">
                             <hr className="mt-2 mb-1" />
                         </div>
                         <div className="row d-flex p-0 m-0 flex-row">
-                            <p className="col-sm-5 text-start p-0 mb-0">Move{props.move3}</p>
-                            <p className="col-sm-4 text-center p-0 mb-0">{props.attack3}</p>
-                            <p className="col-sm-3 text-end p-0 mb-0">{props.accuracy3}</p>
+                            <p className="col-sm-2 text-start p-0 mb-0">Lvl: 30{props.movelvl4}</p>
+                            <p className="col text-start p-0 mb-0">Slash {props.movename4}</p>
+                            <p className="col text-center p-0 mb-0">Power: 70{props.attack4}</p>
+                            <p className="col text-end p-0 mb-0">Acc: 100{props.accuracy4}</p>
                         </div>
-                    </div>
-                    <div className="row px-5 mb-1">
                         <div className="px-0">
-                            <hr className="mt-2 mb-2" />
+                            <hr className="mt-2 mb-1" />
                         </div>
-                        <div className="row d-flex p-0 m-0 flex-row justify-content-center">
-                            <p className="col-sm-4 text-start p-0 mb-0">Height: 4{props.height}</p>
-                            <p className="col-sm-4 text-center p-0 mb-0">Lvl: 1{props.level}</p>
-                            <p className="col-sm-4 text-end p-0 mb-0">Weight: 50{props.weight}</p>
+                        <div className="row d-flex p-0 m-0 flex-row">
+                            <p className="col-sm-2 text-start p-0 mb-0">Lvl: 38{props.movelvl4}</p>
+                            <p className="col text-start p-0 mb-0">Flamethrower {props.movename4}</p>
+                            <p className="col text-center p-0 mb-0">Power: 90{props.attack4}</p>
+                            <p className="col text-end p-0 mb-0">Acc: 100{props.accuracy4}</p>
+                        </div>
+                        <div className="px-0">
+                            <hr className="mt-2 mb-1" />
+                        </div>
+                        <div className="row d-flex p-0 m-0 flex-row">
+                            <p className="col-sm-2 text-start p-0 mb-0">Lvl: 46{props.movelvl4}</p>
+                            <p className="col text-start p-0 mb-0">Fire Spin {props.movename4}</p>
+                            <p className="col text-center p-0 mb-0">Power: 35{props.attack4}</p>
+                            <p className="col text-end p-0 mb-0">Acc: 85{props.accuracy4}</p>
+                        </div>
+                        <div className="px-0">
+                            <hr className="mt-2 mb-1" />
                         </div>
                     </div>
                 </div>
