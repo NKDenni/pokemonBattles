6a348f312c51f1852aafbe48ec0c715b578e3315
Nathan Denni <aubakasan303@gmail.com>
2021-10-21 10:37:59 +0900

    working on adding enemy data

--- client/src/views/stacked.js
+++ client/src/views/stacked.js
@@ -6,7 +6,7 @@ import Learn from '../components/learn';
 import './views.scss';
 
 const SideBySide = (props) => {
-
+    const { player, ptype, phealth, pattack, pdefense, psatt, psdef, pspeed, pflavor } = props;
 
     const [message, setMessage] = useState('');
 
@@ -17,8 +17,8 @@ const SideBySide = (props) => {
                 <Signboard message={message}/>
             </div>
             <div className="stacked col mt-4" style={{ minHeight: `500px`}}>
-                {/* <Battle message={message} setMessage={setMessage}/> */}
-                <Learn message={message} setMessage={setMessage} />
+                <Battle message={message} setMessage={setMessage} player={player} ptype={ptype} phealth={phealth} pattack={pattack} pdefense={pdefense} psatt={psatt} psdef={psdef} pspeed={pspeed} pflavor={pflavor}/>
+                {/* <Learn message={message} setMessage={setMessage} /> */}
             </div>
         </div>
     )
