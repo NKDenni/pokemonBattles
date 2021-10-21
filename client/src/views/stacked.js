51a10beec06fa519bea93a887ef5831be8c944ba
Matias Salas <matias.salas74@gmail.com>
2021-10-20 19:44:09 -0700

    Fix after merge.

--- client/src/views/stacked.js
+++ client/src/views/stacked.js
@@ -9,8 +9,9 @@ const SideBySide = (props) => {
     const { player, ptype, phealth, pattack, pdefense, psatt, psdef, pspeed, pflavor } = props;
 
     const playerBaseStats = {
-        type: "Electric",
-        level: 1
+        ptype: "Electric",
+        level: 1,
+        phealth: 100
     }
 
     const [message, setMessage] = useState('Begin Battle!');
@@ -37,7 +38,9 @@ const SideBySide = (props) => {
                 <Signboard message={message} gameOver={gameOver}/>
             </div>
             <div className="stacked col mt-4" style={{ minHeight: `500px`}}>
-                <Battle handleMessageUpdate= { handleMessageUpdate } handleGameOver={ handleGameOver } updatePlayerStats={ updatePlayerStats } playerStats={ playerStats }/>
+                <Battle handleMessageUpdate= { handleMessageUpdate } handleGameOver={ handleGameOver } updatePlayerStats={ updatePlayerStats } playerStats={ playerStats }
+                    player={player} ptype={ptype} phealth={phealth} pattack={pattack} pdefense={pdefense} psatt={psatt} psdef={psdef} pspeed={pspeed} pflavor={pflavor}
+                />
                 {/* <Battle message={message} setMessage={setMessage} player={player} ptype={ptype} phealth={phealth} pattack={pattack} pdefense={pdefense} psatt={psatt} psdef={psdef} pspeed={pspeed} pflavor={pflavor}/> */}
                 {/* <Learn message={message} setMessage={setMessage} /> */}
             </div>
